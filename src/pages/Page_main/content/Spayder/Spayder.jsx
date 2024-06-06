3eec92a5bba6fb6a7c9c5c54b4491dbc50d2d94e
n1vz18 <erahtin300@gmail.com>
2024-06-06 22:15:31 +0300

    link update

--- src/pages/Page_main/content/Spayder/Spayder.jsx
+++ src/pages/Page_main/content/Spayder/Spayder.jsx
@@ -45,8 +45,8 @@ export default function Spayder(){
                         <motion.p custom={3} variants={textAnimation}>Мы собрали интерактивные задания. Чтобы вам было легче усвоить материал.</motion.p>
                     </div>
                     <div className="promo__rcs">
-                        <motion.a custom={3.5} variants={textAnimation} className="textCdt" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }} href="https://cdt-kanavino.ru/">Сайт МБУ ДО ЦДТ Канавинского района</motion.a>
-                        <motion.a custom={4} variants={textAnimation} className="helloengl" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }} href="https://xn--52-kmc.xn--80aafey1amqq.xn--d1acj3b/program/1405-privet-angliiskii">Записаться в творческое обьединение «Hello, English!»</motion.a>
+                        <motion.a custom={3.5} variants={textAnimation} className="textCdt" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }} target="_blank"  href="https://cdt-kanavino.ru/">Сайт МБУ ДО ЦДТ Канавинского района</motion.a>
+                        <motion.a custom={4} variants={textAnimation} className="helloengl" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }} target="_blank" href="https://xn--52-kmc.xn--80aafey1amqq.xn--d1acj3b/program/1405-privet-angliiskii">Записаться в творческое обьединение «Hello, English!»</motion.a>
                     </div>
                 </div>
             </div>
